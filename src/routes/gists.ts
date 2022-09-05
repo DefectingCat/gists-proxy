ace74216bf18b62934b02892aabb949d27ed2ac2
DefectingCat <i@defect.ink>
2022-09-05 02:57:52 +0000

    add cache

--- src/routes/gists.ts
+++ src/routes/gists.ts
@@ -1,16 +1,29 @@
-import { Router } from 'express';
+import { Router, Request } from 'express';
 import $ax from '../utils/axios';
+import redis from '../utils/redis';
 
 const router = Router();
+const getData = async (req: Request) => {
+    const { params, method } = req;
+    const { host, ...headers } = req.headers;
+    const key = `${req.path}${JSON.stringify(params)}`;
+    const cache = await redis.get(key);
+    if (cache) return JSON.parse(cache);
+
+    const result = (
+        await $ax(req.path, {
+            headers: headers as {},
+            params,
+            method,
+        })
+    ).data;
+    await redis.setex(key, 600, JSON.stringify(result));
+    return result;
+};
 
 router.get('/*', async (req, res) => {
     try {
-        const { params } = req;
-        const { host, ...headers } = req.headers;
-        const result = await $ax.get(req.path, {
-            headers: headers as {},
-            params,
-        });
+        const result = await getData(req);
         console.log(result);
     } catch (err) {
         console.error(err);
