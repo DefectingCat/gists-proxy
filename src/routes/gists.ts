5288a2d511b2120cf93550299c90cb7aa370cc91
DefectingCat <i@defect.ink>
2022-09-05 03:33:23 +0000

    update proxy

--- src/routes/gists.ts
+++ src/routes/gists.ts
@@ -4,31 +4,45 @@ import redis from '../utils/redis';
 
 const router = Router();
 const getData = async (req: Request) => {
-    const { params, method } = req;
+    const { query, method } = req;
     const { host, ...headers } = req.headers;
-    const key = `${req.path}${JSON.stringify(params)}`;
+    const key = `${req.path}${JSON.stringify(query)}`;
+    const headerKey = `${key}-headers`;
     const cache = await redis.get(key);
-    if (cache) return JSON.parse(cache);
+    if (cache) {
+        const headers = await redis.get(headerKey);
+        if (!headers) throw new Error('no headrs');
+        return {
+            headers,
+            data: cache,
+        };
+    }
 
-    const result = (
-        await $ax(req.path, {
-            headers: headers as {},
-            params,
-            method,
-        })
-    ).data;
-    await redis.setex(key, 600, JSON.stringify(result));
-    return result;
+    const result = await $ax(req.path, {
+        headers: headers as {},
+        params: query,
+
+        method,
+    });
+    await redis.setex(key, 600, JSON.stringify(result.data));
+    await redis.setex(headerKey, 600, JSON.stringify(result.headers));
+    return {
+        headers: JSON.stringify(result.headers),
+        data: result.data,
+    };
 };
 
-router.get('/*', async (req, res) => {
+router.all('/*', async (req, res) => {
     try {
         const result = await getData(req);
-        console.log(result);
+        res.header(JSON.parse(result.headers));
+        res.setHeader('content-length', result.data.length);
+        res.setHeader('transfer-encoding', '');
+        res.send(result.data);
     } catch (err) {
         console.error(err);
+        res.status(404).send(JSON.stringify(err));
     }
-    res.send('Hello! gists');
 });
 
 export default router;
